0c7a4c17787a6d5a0e4f7319d088ac503fd94038
2rjswn <h47841930@gmail.com>
2024-04-20 23:34:24 +0900

    security

--- src/main/java/com/example/springsecuritybasic/config/BankUserDetails.java
+++ src/main/java/com/example/springsecuritybasic/config/BankUserDetails.java
@@ -1,19 +1,35 @@
 package com.example.springsecuritybasic.config;
 
+import com.example.springsecuritybasic.model.Customer;
 import com.example.springsecuritybasic.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
 public class BankUserDetails implements UserDetailsService {
     private final CustomerRepository customerRepository;
 
-    public BankUserDetails(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        String userName, password;
+        List<GrantedAuthority> authorities;
+        List<Customer> customer = customerRepository.findByEmail(username);
+        if (customer.size() == 0) {
+            throw new UsernameNotFoundException("User details not found for the user : " + username);
+        } else{
+            userName = customer.get(0).getEmail();
+            password = customer.get(0).getPwd();
+            authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
+        }
+        return new User(userName,password,authorities);
     }
 }
