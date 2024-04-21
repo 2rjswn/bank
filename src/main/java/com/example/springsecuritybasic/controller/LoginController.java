190a1d74fab60467073931b7187cc8d32cb4af3a
2rjswn <h47841930@gmail.com>
2024-04-21 23:33:33 +0900

    security

--- src/main/java/com/example/springsecuritybasic/controller/LoginController.java
+++ src/main/java/com/example/springsecuritybasic/controller/LoginController.java
@@ -1,7 +1,12 @@
 package com.example.springsecuritybasic.controller;
 
+import com.example.springsecuritybasic.model.Customer;
 import com.example.springsecuritybasic.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
     private final CustomerRepository customerRepository;
 
-
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
+        Customer savedCustomer = null;
+        ResponseEntity response = null;
+        try {
+            savedCustomer = customerRepository.save(customer);
+            if (savedCustomer.getId() > 0) {
+                response = ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body("Given user details are successfully registered");
+            }
+        } catch (Exception ex) {
+            response = ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception occured due to " + ex.getMessage());
+        }
+        return response;
+    }
 }
