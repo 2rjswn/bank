7948ffd8430163026def9577a2a10ff3a8d5d606
2rjswn <h47841930@gmail.com>
2024-04-29 20:14:25 +0900

    security

--- src/main/java/com/example/springsecuritybasic/controller/AccountController.java
+++ src/main/java/com/example/springsecuritybasic/controller/AccountController.java
@@ -1,12 +1,24 @@
 package com.example.springsecuritybasic.controller;
 
+import com.example.springsecuritybasic.model.Accounts;
+import com.example.springsecuritybasic.repository.AccountsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
+    @Autowired
+    private AccountsRepository accountsRepository;
+
     @GetMapping("/myAccount")
-    public String getAccountDetails() {
-        return "dd";
+    public Accounts getAccountDetails(@RequestParam int id) {
+        Accounts accounts = accountsRepository.findByCustomerId(id);
+        if (accounts != null ) {
+            return accounts;
+        }else {
+            return null;
+        }
     }
 }
