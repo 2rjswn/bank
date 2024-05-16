f08d7179084efd0591a6fdd2992cdcc4ba97b3d6
2rjswn <h47841930@gmail.com>
2024-05-16 17:57:09 +0900

    security

--- src/main/java/com/example/springsecuritybasic/controller/ContectController.java
+++ src/main/java/com/example/springsecuritybasic/controller/ContectController.java
@@ -3,12 +3,14 @@ package com.example.springsecuritybasic.controller;
 import com.example.springsecuritybasic.model.Contact;
 import com.example.springsecuritybasic.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @RestController
@@ -17,10 +19,15 @@ public class ContectController {
     private ContactRepository contactRepository;
 
     @PostMapping("/contact")
-    public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
+    @PostFilter("filterObject.contactName != 'Test'")
+    public List<Contact> saveContactInquiryDetails(@RequestBody List<Contact> contacts) {
+        Contact contact = contacts.get(0);
         contact.setContactId(getServiceReqNumber());
         contact.setCreateDt(new Date(System.currentTimeMillis()));
-        return contactRepository.save(contact);
+        contact = contactRepository.save(contact);
+        List<Contact> returnContacts = new ArrayList<>();
+        returnContacts.add(contact);
+        return returnContacts;
     }
 
     public String getServiceReqNumber() {
